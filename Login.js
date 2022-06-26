d4fdd82e6d6deee501c55662790a3768f3b80383
nechamish <nechami77@gmail.com>
2022-06-26 12:57:19 +0300

    finishproject

---
+++ Login.js
@@ -0,0 +1,23 @@
+function Enter() {
+  var mail = document.getElementById("mail").value;
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", "http://localhost:3000/users");
+  xhr.send();
+  xhr.onload = function () {
+    if (xhr.status != 200) {
+      alert(`Error ${xhr.status}: ${xhr.statusText}`);
+    } else {
+      let users = JSON.parse(xhr.responseText);
+      if (users[0].email === mail) {
+        localStorage.setItem("U", JSON.stringify(users));
+        window.location.href = "Manager.html";
+      } else {
+        var CurentUser = users.find((u) => u.email === mail);
+        if (CurentUser != null) {
+          localStorage.setItem("U", JSON.stringify(CurentUser));
+          window.location.href = "User.html";
+        }
+      }
+    }
+  };
+}
