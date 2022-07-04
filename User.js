ea2afa3d37e48f03e3a30b9efb191e57ed8fec00
nechamish <nechami77@gmail.com>
2022-07-04 15:28:35 +0300

    user.jsupdate

--- User.js
+++ User.js
@@ -1,6 +1,4 @@
 function Data() {
-  // const params = new URLSearchParams(window.location.search);
-  // const id = params.get("userId");
   const s = localStorage["U"];
   var mtDate = JSON.parse(s);
   var v1 = mtDate.firstName;
@@ -10,7 +8,6 @@ function Data() {
   var v5 = mtDate.phone;
   var v6 = mtDate.hight;
   var v7 = mtDate.weight;
- 
   document.getElementById("name").value = v1;
   document.getElementById("lastname").value = v2;
   document.getElementById("Adress").value =
@@ -19,17 +16,24 @@ function Data() {
   document.getElementById("email1").value = v3;
   document.getElementById("height").value = v6;
   document.getElementById("weight").value = v7.startWeight;
+//  document.getElementById("met1").value = "התאריך"+" "+
+//     v7.meeting[0].date + " " +"המשקל"+ v7.meeting[0].weight;
+//    document.getElementById("met1").value =
+//      "התאריך" + " " + v7.meeting[1].date + " " + "המשקל" + v7.meeting[1].weight;;
+//       document.getElementById("met1").value = v7.meeting[2];
+  
 }
 
 function Bmi() {
   let h = document.getElementById("height").value;
   let w = document.getElementById("weight").value;
-  document.getElementById("cal").value = w / (h ^ 2);
+  document.getElementById("cal").value = w / (h ** 2);
 }
 
-function openUrl()
-{
- const params = new URLSearchParams(window.location.search);
- const id = params.get("userId");
- window.location.href = `MyDairy?userId=${id}`;
+
+
+function openUrl() {
+  const params = new URLSearchParams(window.location.search);
+  const id = params.get("userId");
+  window.location.href = `MyDairy.html?userId=${id}`;
 }
